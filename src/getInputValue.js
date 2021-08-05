5897b325e9a69f62fe82fc713067783722199fa4
vlashchanka <vitalij.lashenko@gmail.com>
2021-08-05 20:08:59 +0300

    Save the progress

---
+++ src/getInputValue.js
@@ -0,0 +1,3 @@
+import { pathOr, trim, pipe } from 'ramda';
+
+export default pipe(pathOr('', ['target', 'value']), trim);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { pipe, tap, ifElse, isEmpty, curry } from 'ramda';
+import getInputValue from './getInputValue';
+import getUrl from './getUrl';
+import Results from './Result';
+
+const render = markup => {
+    const resultsElement = document.getElementById('results');
+
+    resultsElement.innerHTML = markup;
+};
+
+const renderSearchResults = pipe(Results, render);
+const renderSearchNoResults = pipe(noResultReason => [noResultReason, [], [], []], renderSearchResults);
+
+const searchAndRenderResults = pipe(
+    getUrl,
+    url =>
+        fetch(url)
+            .then(res => res.json())
+            .then(tap(console.warn))
+            .then(renderSearchResults)
+            .catch(renderSearchNoResults)
+);
+
+const makeSearchRequestIfValid = pipe(
+    getInputValue,
+    ifElse(isEmpty, renderSearchNoResults, searchAndRenderResults),
+
+);
+
+const inputElement = document.querySelector('input');
+
+inputElement.addEventListener('keyup', makeSearchRequestIfValid);
